24d79b9cf2b8ec4098f0159057abe32c3e8f8bf4
Jason Ko <jekb2019@gmail.com>
2021-10-22 16:10:48 +1300

    Add: theme fore gallery

--- navigation/GalleryTab.js
+++ navigation/GalleryTab.js
@@ -1,44 +1,62 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components/native';
 import GoalGallery from '../screens/gallery/GoalGallery';
 import LifeGallery from '../screens/gallery/LifeGallery';
 import WorkGallery from '../screens/gallery/WorkGallery';
 import { SCREEN_NAMES } from '../util/constants';
+import { capitalizeFirstLetter } from '../util/helpers';
 import { displayIcon } from './util/displayer';
 
 const Tab = createBottomTabNavigator();
 
-const GalleryTab = (props) => (
-  <Tab.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <Tab.Screen
-      component={WorkGallery}
-      name={SCREEN_NAMES.WORK}
-      options={{
-        tabBarIcon: ({ focused, color, size }) =>
-          displayIcon(SCREEN_NAMES.WORK, color, size),
+const GalleryTab = (props) => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: theme.tabBg,
+        },
+        tabBarActiveTintColor: theme.tabActive,
+        tabBarInactiveTintColor: theme.tabInactive,
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
       }}
-    />
-    <Tab.Screen
-      component={LifeGallery}
-      name={SCREEN_NAMES.LIFE}
-      options={{
-        tabBarIcon: ({ focused, color, size }) =>
-          displayIcon(SCREEN_NAMES.LIFE, color, size),
-      }}
-    />
-    <Tab.Screen
-      component={GoalGallery}
-      name={SCREEN_NAMES.GOAL}
-      options={{
-        tabBarIcon: ({ focused, color, size }) =>
-          displayIcon(SCREEN_NAMES.GOAL, color, size),
-      }}
-    />
-  </Tab.Navigator>
-);
+    >
+      <Tab.Screen
+        component={WorkGallery}
+        name={SCREEN_NAMES.WORK}
+        options={{
+          tabBarIcon: ({ _, color, size }) =>
+            displayIcon(SCREEN_NAMES.WORK, color, size),
+          tabBarLabel: capitalizeFirstLetter(SCREEN_NAMES.WORK),
+        }}
+      />
+      <Tab.Screen
+        component={LifeGallery}
+        name={SCREEN_NAMES.LIFE}
+        options={{
+          tabBarIcon: ({ _, color, size }) =>
+            displayIcon(SCREEN_NAMES.LIFE, color, size),
+          tabBarLabel: capitalizeFirstLetter(SCREEN_NAMES.LIFE),
+        }}
+      />
+      <Tab.Screen
+        component={GoalGallery}
+        name={SCREEN_NAMES.GOAL}
+        options={{
+          tabBarIcon: ({ _, color, size }) => {
+            return displayIcon(SCREEN_NAMES.GOAL, color, size);
+          },
+          tabBarLabel: capitalizeFirstLetter(SCREEN_NAMES.GOAL),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default GalleryTab;
